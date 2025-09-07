117f5ff9da9aef251210912b977875bb9622b575
amonroca <roc23009@byui.edu>
2025-09-07 10:56:39 -0300

    feat: adicionar suporte ao vite-plugin-static-copy e ajustar configuração do Vite

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,7 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  //const cart = getLocalStorage("so-cart") || [];
-  const cartItems = Array.isArray(getLocalStorage("so-cart")) ? getLocalStorage("so-cart") : [];
+  const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
